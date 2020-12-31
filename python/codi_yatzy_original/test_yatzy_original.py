a189537e74401287ea26d92444693b59df19812d
OualidZM <ozaaj@cifpfbmoll.eu>
2020-12-31 08:48:06 +0100

    docs: :memo: test_yatzy_original

--- python/codi_yatzy_original/test_yatzy_original.py
+++ python/codi_yatzy_original/test_yatzy_original.py
@@ -9,7 +9,6 @@ def test_chance_scores_sum_of_all_dice():
         assert expected == actual
         assert 16 == Yatzy.chance(3,3,4,5,1)
   
-
 def test_yatzy_scores_50():
         expected = 50
         actual = Yatzy.yatzy([4,4,4,4,4])
